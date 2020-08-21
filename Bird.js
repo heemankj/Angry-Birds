281af1d2cccad618844a9d57b5e27d8059e43e1e
heemankj <67792670+heemankj@users.noreply.github.com>
2020-08-21 19:39:12 +0530

    Add files via upload

--- Bird.js
+++ Bird.js
@@ -2,11 +2,22 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.image1 = loadImage("sprites/smoke.png");
+    this.path=[]
   }
 
   display() {
     //this.body.position.x = mouseX;
     //this.body.position.y = mouseY;
     super.display();
+    var pos=this.body.position;
+    if(this.body.velocity.x>10&&this.body.position.x>200){
+    var xy=[pos.x,pos.y]
+    this.path.push(xy)
+  }
+  
+    for(var i=0;i<this.path.length;i++){
+      image(this.image1,this.path[i][0],this.path[i][1]);  
+    }
   }
 }
